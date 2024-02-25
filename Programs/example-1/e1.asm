4c7e15cb61bfd375ca5dce72ef6c8dc3b40df67e
Jonas Almås <jonasalmaas7@gmail.com>
2024-02-25 20:28:20 +0100

    Create an example program

---
+++ Programs/example-1/e1.asm
@@ -0,0 +1,9 @@
+; This program adds 5 to Register A in an infinite loop and outputs the result
+
+LDI 5		; Load 5 into Register A
+STA 255		; Store the value in the A Register into RAM at address 255
+
+loop:
+ADD 255   	; Add the value at memory address 15 to Register A	
+OUT   		; Output the value in Register A
+JMP loop	; Jump up to the "loop" lable to continue the loop
