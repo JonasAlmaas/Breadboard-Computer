80d0493711bbe97d5177abe2a7379fa55cd2f3be
Jonas Almås <jonasalmaas7@gmail.com>
2024-02-28 22:12:48 +0100

    m

--- Programs/example-1/e1.asm
+++ Programs/example-1/e1.asm
@@ -6,4 +6,4 @@ STA 255		; Store the value in the A Register into RAM at address 255
 loop:
 ADD 255   	; Add the value at memory address 15 to Register A	
 OUT   		; Output the value in Register A
-JMP loop	; Jump up to the "loop" lable to continue the loop
+JMP loop	; Jump up to the "loop" label to continue the loop
