82b48fe097b5be701ff100af6643764da9a9f70c
Jonas Almaas <jonas.f.almaas@gmail.com>
2024-03-16 21:35:34 +0100

    Example 1

--- Programs/example-1/e1.asm
+++ Programs/example-1/e1.asm
@@ -4,6 +4,6 @@ LDI 5		; Load 5 into Register A
 STA 255		; Store the value in the A Register into RAM at address 255
 
 loop:
-ADD 255   	; Add the value at memory address 15 to Register A	
+ADD 255   	; Add the value at memory address 255 to Register A	
 OUT   		; Output the value in Register A
 JMP loop	; Jump up to the "loop" label to continue the loop
