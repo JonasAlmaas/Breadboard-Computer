61615e142310ba15e79798c20037048749d2db22
Jonas Almaas <jonas.f.almaas@gmail.com>
2024-03-17 16:55:48 +0100

    Updated example code

--- Programs/example-1/_upload.bat
+++ Programs/example-1/_upload.bat
@@ -1,2 +1,3 @@
 cd ../bin
+Assembler.exe -f B ../example-1/e1.asm
 Programmer.exe -p COM3 ../example-1/e1.bin
--- Programs/example-1/e1.asm
+++ Programs/example-1/e1.asm
@@ -1,10 +1,10 @@
-; This program adds 5 to Register A in an infinite loop and outputs the result
+; This program adds 2 to Register A in an infinite loop and outputs the result
 
-LDI 5       ; Load 5 into Register A
+NOP         ; This has to be here, because something is wrong ...
+LDI 2       ; Load 2 into Register A
 STA 255     ; Store the value in the A Register into RAM at address 255
-OUT         ; Output the value in Register A
 
 loop:
-ADD 255     ; Add the value at memory address 255 to Register A	
 OUT         ; Output the value in Register A
+ADD 255     ; Add the value at memory address 255 to Register A	
 JMP loop    ; Jump up to the "loop" label to continue the loop
